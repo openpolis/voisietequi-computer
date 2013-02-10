5cbf0c7ff24d4fa68e7d828e40fd6b9643181cad
joke2k <joke2k@gmail.com>
2013-02-10 18:34:38 +0100

    add wants_newsletter field in user_data. advanced logging with external configuration file. fix md5 calculation to avoid unicode chars

--- computer/helpers.py
+++ computer/helpers.py
@@ -1,13 +1,14 @@
 import re
 import hashlib
 from datetime import datetime
+from random import randint
 
 def regexp(rexp, value):
     rexp = re.compile(rexp)
     return bool(rexp.match(value))
 
-def md5(salt):
+def md5():
     m = hashlib.md5()
-    m.update("{0}{1}".format(datetime.now(),salt))
+    m.update("{0}{1}".format(datetime.now(),randint(1000,999999999)))
     return m.hexdigest()
 
